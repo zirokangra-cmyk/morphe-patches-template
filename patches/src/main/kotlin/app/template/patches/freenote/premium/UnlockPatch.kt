6f0b257cf4048e621938fe06d305105e70bb3562
zirokangra-cmyk <zirokangra@gmail.com>
2026-09-15 18:51:03 +0530

    Update UnlockPatch.kt

--- patches/src/main/kotlin/app/template/patches/freenote/premium/UnlockPatch.kt
+++ patches/src/main/kotlin/app/template/patches/freenote/premium/UnlockPatch.kt
@@ -1,8 +1,15 @@
 package app.template.patches.freenote.premium
-import app.morphe.patcher.patch.bytecodePatch
 
+import app.morphe.patcher.patch.bytecodePatch
+import app.template.patches.shared.Constants.COMPATIBILITY_FREENOTE
+
+@Suppress("unused")
 val unlockPatch = bytecodePatch(
     name = "Unlock Premium",
     description = "Unlocks premium features"
 ) {
+    compatibleWith(COMPATIBILITY_FREENOTE)
+
+    execute {
+    }
 }
