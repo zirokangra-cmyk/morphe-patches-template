f43c893517b8039e1b191da0cdf8683b3088809c
zirokangra-cmyk <zirokangra@gmail.com>
2026-09-15 18:50:23 +0530

    Update RemoveAds.kt

--- patches/src/main/kotlin/app/template/patches/freenote/ads/RemoveAds.kt
+++ patches/src/main/kotlin/app/template/patches/freenote/ads/RemoveAds.kt
@@ -1,8 +1,15 @@
 package app.template.patches.freenote.ads
-import app.morphe.patcher.patch.bytecodePatch
 
+import app.morphe.patcher.patch.bytecodePatch
+import app.template.patches.shared.Constants.COMPATIBILITY_FREENOTE
+
+@Suppress("unused")
 val removeAdsPatch = bytecodePatch(
     name = "Remove Ads",
     description = "Removes ads"
 ) {
+    compatibleWith(COMPATIBILITY_FREENOTE)
+
+    execute {
+    }
 }
